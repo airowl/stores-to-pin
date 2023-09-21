d57a677ca978d7491456e9e0aa324d6046708869
Airowl <airowl.gasga@gmail.com>
2023-09-21 16:25:48 +0200

    fix

--- index.js
+++ index.js
@@ -22,43 +22,67 @@ function upload() {
    
             const data = e.target.result;
             const excel = XLSX.read(data, {
-                type : 'binary' 
+                type : 'binary',
+                cellDates:true,
+                cellTimes: true
             });
             let  result = {};
             excel.SheetNames.forEach(function(sheetName) {
                 var roa = XLSX.utils.sheet_to_row_object_array(excel.Sheets[sheetName]);
                 if (roa.length > 0) {
                     result = roa.map(r => {
+                        
 
-                        let { title, location, link, address, caption, coords} = r;
+                        let { title, coords, where, time} = r;
 
                         let obj = {
                             title: "",
-                            location: "",
-                            caption: "",
-                            link: "",
                             coords: {
                                 lat: 0,
                                 lng: 0            
-                            }
+                            },
+                            where: "",
+                            time: "",
+                            date: "",
+                            caption: ''
                         };
 
-                        obj.title = title;
-            
-                        if(location) {
-                            obj.location = location;
-                        }else {
-                            delete obj.location
+                        obj.title = title ?? '';
+
+                        if(typeof coords !== 'undefined'){
+                            const c = coords.split(',');
+                            obj.coords.lat = c[0];
+                            obj.coords.lng = c[1];
                         }
 
-                        if(link) {
-                            obj.link = link
-                        }else {
-                            delete obj.link
-                        }
-                        obj.caption = `${address || ''}<br/>${caption || ''}`;
-                        obj.coords.lat = getRandomFloat(45.4500, 45.5000, 4);
-                        obj.coords.lng = getRandomFloat(9.1500, 9.2500, 4);
+                        console.log(r);
+                        obj.where = where ?? '';
+
+                        console.log(r.date);
+                        const date = new Date(r.date);
+                        const d = date.getDate() + '-' + date.getMonth() + '-' + date.getFullYear();
+                        obj.date = d;
+            
+                        obj.time = time;
+
+                        obj.caption = where + ', ' + d + ', ' + time;
+
+                        
+
+                        //if(location) {
+                        //    obj.location = location;
+                        //}else {
+                        //    delete obj.location
+                        //}
+
+                        //if(link) {
+                        //    obj.link = link
+                        //}else {
+                        //    delete obj.link
+                        //}
+                        //obj.caption = `${address || ''}<br/>${caption || ''}`;
+                        //obj.coords.lat = getRandomFloat(45.4500, 45.5000, 4);
+                        //obj.coords.lng = getRandomFloat(9.1500, 9.2500, 4);
         
                         return obj;
                     }); 
